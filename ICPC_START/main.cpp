40d49a0e4ace5e83f01c540de0c6ab90625b8715
Jack <lplcx@live.com>
2017-04-30 16:34:49 +0800

    Initial Commit

---
+++ ICPC_START/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ICPC_START
+//
+//  Created by xin chen on 30/04/2017.
+//  Copyright © 2017 xin chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
