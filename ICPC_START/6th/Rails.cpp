e2922025edb88b9d18f31cc858dff2daee028620
Jack <lplcx@live.com>
2017-04-30 17:08:32 +0800

    判断车厢出顺序是否可行 例6-2

---
+++ ICPC_START/6th/Rails.cpp
@@ -0,0 +1,9 @@
+//
+//  Rails.cpp
+//  ICPC_START
+//
+//  Created by xin chen on 30/04/2017.
+//  Copyright © 2017 xin chen. All rights reserved.
+//
+
+#include "Rails.hpp"
---
+++ ICPC_START/6th/Rails.hpp
@@ -0,0 +1,14 @@
+//
+//  Rails.hpp
+//  ICPC_START
+//
+//  Created by xin chen on 30/04/2017.
+//  Copyright © 2017 xin chen. All rights reserved.
+//
+
+#ifndef Rails_hpp
+#define Rails_hpp
+
+#include <stdio.h>
+
+#endif /* Rails_hpp */
